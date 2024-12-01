7a3c3bcb55808d8f55e0b92794f2a1576103f2b5
webcrafter000 <nagendranalam1611@gmail.com>
2024-12-01 21:09:16 +0530

    initial

--- app/(root)/(home)/upcoming/page.tsx
+++ app/(root)/(home)/upcoming/page.tsx
@@ -4,7 +4,6 @@ const UpcomingPage = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <h1 className="text-3xl font-bold">Upcoming Meeting</h1>
-
       <CallList type="upcoming" />
     </section>
   );
